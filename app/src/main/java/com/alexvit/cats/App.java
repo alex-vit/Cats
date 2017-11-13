eb5dfee80b5aff67172913c044956bbeb247e4eb
Alexander Vitjukov <ryujo.la@gmail.com>
2017-11-13 16:10:56 +0200

    Add Firebase crash reporting

--- app/src/main/java/com/alexvit/cats/App.java
+++ app/src/main/java/com/alexvit/cats/App.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import com.alexvit.cats.di.component.ApplicationComponent;
 import com.alexvit.cats.di.component.DaggerApplicationComponent;
 import com.alexvit.cats.di.module.ContextModule;
+import com.google.firebase.FirebaseApp;
 
 /**
  * Created by Aleksandrs Vitjukovs on 11/4/2017.
@@ -22,11 +23,17 @@ public class App extends Application {
         component = DaggerApplicationComponent.builder()
                 .contextModule(new ContextModule(this))
                 .build();
+
+        initFirebase();
     }
 
     public static ApplicationComponent component(Context context) {
         App app = (App) context.getApplicationContext();
         return app.component;
+    }
+
+    private void initFirebase() {
+        FirebaseApp.initializeApp(this);
     }
 
 }
