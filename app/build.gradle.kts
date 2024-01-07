92f2d221d5dc5b4b08534b2784891f6e561b9164
alex <a.vitjukovs@gmail.com>
2024-01-07 15:56:29 +0200

    fix release crash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.alexvit.cats"
         minSdk = 21
         targetSdk = 34
-        versionCode = 10103
-        versionName = "1.1.2"
+        versionCode = 10104
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
@@ -32,6 +32,8 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+//            signingConfig = signingConfigs.getByName("debug")
+//            isDebuggable = true
         }
     }
     buildFeatures { buildConfig = true }
